2126e8521439a2e87f69d9db2ef4cdd844263d9e
Yair Garizabalo <yjgarizabalo@gmail.com>
2019-02-09 19:56:37 -0500

    update - img -  loginjs

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
 import React from 'react';
 import { 
-  StyleSheet, 
-  Text, 
-  View 
+  StyleSheet,  
+  View,
+  Text,
+  ImageBackground,
+  Image 
   } from 'react-native';
 
-  import Login from '../src/components/Login'
-
+import Login from './src/components/login'
+import bgLogin from './assets/img/bglogin.jpg'
+import Logo from './assets/img/logo.png'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <Login/>    
-      </View>
+      <ImageBackground source={bgLogin} style={styles.bgContainer}>
+        <View style={styles.logoContainer}>
+          <Image source={Logo}/>
+        </View>
+        <View style={styles.textLogoContainer}>
+         <Text style={styles.logoText}>El mejor monitor de calificaciones</Text>
+        </View>
+      
+       <Login/>
+      </ImageBackground>
+
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  bgContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center', 
+  },
+  
+  logoText: {
+    color: '#383838',
+    fontSize: 16,
+    fontWeight: '500',
+    textAlign: 'center'
+  },
+
+})
 
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,134 @@
-import React from 'reacta'
-import {StyleSheet, Text, View} from 'react-native'
+import React, {Component} from 'react'
+import {
+  StyleSheet,
+  Text, 
+  View,
+  TextInput,
+  Dimensions,
+  TouchableOpacity
+} from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
 
-class Login extends Compounent {
+const { width: WIDTH } = Dimensions.get('window')
+
+class Login extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      showPass: true,
+      press: false
+    }
+  } 
+  showPass = () => {
+    if(this.state.press == false) {
+      this.setState({showPass: false, press: true})
+    }else
+     this.setState({showPass: true, press: false})
+  } 
   render() {
     return(
-      <View style={styles.Container}>
-        <Text>login</Text>
-      </View>
+      <View  style={styles.containerLogin}>
+        <View style={styles.inputContainer}>
+            <Ionicons name='ios-person' size={28} color={'#383838'} style={styles.inputIcon}/>
+              <TextInput
+                style={styles.input}
+                placeholder={'Usuario o Email'}
+                placeholderTextColor={'#383838'}
+                underlineColorAndroid='transparent'
+              />
+            </View>
+    
+            <View style={styles.inputContainer}>
+            <Ionicons name='md-lock' size={28} color={'#383838'} style={styles.inputIcon}/>
+              <TextInput
+                style={styles.input}
+                placeholder={'Contraseña'}
+                secureTextEntry={this.state.showPass}
+                placeholderTextColor={'#383838'}
+                underlineColorAndroid='transparent'
+              />
+    
+            <TouchableOpacity style={styles.btnEye} onPress={this.showPass.bind(this)}>
+              <Ionicons name={this.state.showPass == false ? 'ios-eye' : 'ios-eye-off'} size={26} color={'#383838'}/>
+            </TouchableOpacity>
+            </View>
+
+            <View style={styles.btnContainer}> 
+              <TouchableOpacity style={styles.btnLogin}>
+                <Text style={styles.text}>Enviar</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={styles.btnFacebook}>
+                <Text style={styles.text}>Iniciar Con Facebook</Text>
+              </TouchableOpacity>
+            </View>
+        </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  Container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  containerLogin: {
+    marginTop: 40,
   },
+  inputContainer: {
+    marginTop: 10
+  },
+  
+  input: {
+    width: WIDTH -55,
+    height: 45,
+    borderRadius: 6,
+    paddingLeft: 45,
+    backgroundColor: 'rgba(224, 224, 224, 0.6)',
+    color: '#383838',
+    marginHorizontal: 25
+  },
+
+  inputIcon: {
+    position: 'absolute',
+    top: 8,
+    left: 37
+  },
+  
+  btnEye: {
+    position: 'absolute',
+    top: 8,
+    right: 37 
+  },
+
+
+  btnContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  btnLogin: {
+    width: WIDTH -55,
+    height: 45,
+    borderRadius: 6,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(137, 228, 173, 0.8)',
+    marginTop: 20
+  },
+
+  btnFacebook: {
+    width: WIDTH -55,
+    height: 45,
+    borderRadius: 6,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(59, 89, 152, 0.8)',
+    marginTop: 20
+  },
+
+  text: {
+    color: 'rgba(255, 255, 255, 0.7)',
+    textAlign: 'center',
+    fontSize: 17
+  }
+
 })
 
 export default Login
